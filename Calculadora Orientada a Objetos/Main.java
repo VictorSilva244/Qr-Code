73176b93852fdbb2a99f0f2335b252ffc0b51f1c
Victor Silva <victor.silva37@outlook.com>
2022-11-16 02:26:36 -0300

    Melhor apresentação de código

--- Calculadora Orientada a Objetos/Main.java
+++ Calculadora Orientada a Objetos/Main.java
@@ -7,11 +7,3 @@ class Main {
   }
 }
 
-/*
-  Modo de uso:
-  Cada elemento deve ser separado por (enter) um do outro.
-  Por exemplo: 7 (enter) + (enter) 8 (enter).
-  Caso a primeira entrada for uma operação (+,-,*,/) e a segunda for um numero, o programa vai utilizar o resultado da conta anterior. Nas entradas "+" e "9", o programa vai utilizar o resultado anterior e somar com 9.
-  Ao digitar "historico" quando o programa pedir uma operação ele vai imprimir todas as contas até agora.
-  ao digitar "exit" quando o programa pedir uma operação você encerra o programa.
-*/
